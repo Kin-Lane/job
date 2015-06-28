7095d95c52950a0714d7dc06d28f0a5ed7f90fb1
Kin Lane <info@apievangelist.com>
2015-06-28 00:23:39 -0700

    remove some includes

--- api/methods/platform.php
+++ api/methods/platform.php
@@ -1,28 +1,21 @@
 <?php
 
-require_once('m-blog-get.php');
-require_once('m-blog-post.php');
+require_once('m-job-get.php');
+require_once('m-job-post.php');
 
-require_once('m-blog-published-get.php');
-require_once('m-blog-draft-get.php');
+require_once('m-job-tags-get.php');;
+require_once('m-job-tags-tag-delete.php');
 
-require_once('m-blog-byweek-get.php');
+require_once('m-job-tags-tag-job-get.php');
 
-require_once('m-blog-tags-get.php');
-require_once('m-blog-tags-byweek-get.php');
-require_once('m-blog-tags-tag-delete.php');
+require_once('m-job-job_id-get.php');
 
-require_once('m-blog-tags-tag-blog-get.php');
-require_once('m-blog-tags-tag-build-get.php');
+require_once('m-job-job_id-tags-tag-get.php');
+require_once('m-job-job_id-tags-get.php');
 
-require_once('m-blog-blog_id-get.php');
+require_once('m-job-job_id-tags-post.php');
 
-require_once('m-blog-blog_id-tags-tag-get.php');
-require_once('m-blog-blog_id-tags-get.php');
-
-require_once('m-blog-blog_id-tags-post.php');
-
-require_once('m-blog-blog_id-put.php');
-require_once('m-blog-blog_id-delete.php');
+require_once('m-job-job_id-put.php');
+require_once('m-job-job_id-delete.php');
 
 ?>
